77cc143f4eddc1625abda619f3d20c06ff0e9907
Florian Thiévent <florian.thievent@sbb.ch>
2020-12-21 18:11:18 +0100

    added index

--- counter/models.py
+++ counter/models.py
@@ -6,6 +6,10 @@ class Visits(models.Model):
     class Meta:
         verbose_name = 'Visit'
         verbose_name_plural = 'Visits'
+        indexes = [
+            models.Index(fields=['id', 'added'])
+        ]
+
     current_loggedin = models.BigIntegerField()
     current_free = models.BigIntegerField()
     added = models.DateTimeField(auto_now_add=True)
